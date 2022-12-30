1b5586e2a45dab459838e8fc3fa979c1a076c701
Prime-png <Primenotfound@gmail.com>
2022-12-29 22:41:09 -0600

    started to make the Items class

---
+++ src/main/java/net/prime/bettervillages/item/ModItems.java
@@ -0,0 +1,13 @@
+package net.prime.bettervillages.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.prime.bettervillages.BetterVillages;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+                DeferredRegister.create(ForgeRegistries.ITEMS, BetterVillages.MOD_ID);
+
+
+}
