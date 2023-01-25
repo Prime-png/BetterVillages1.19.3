d44f1132aaae7612f33f9b987bea35d931b5ccb3
Prime-png <Primenotfound@gmail.com>
2023-01-25 15:01:51 -0600

    added a (temporary?) Item. Zircon as well as a TEMPORARY texture zircon.png

--- src/main/java/net/prime/bettervillages/item/ModItems.java
+++ src/main/java/net/prime/bettervillages/item/ModItems.java
@@ -1,13 +1,22 @@
 package net.prime.bettervillages.item;
 
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import net.prime.bettervillages.BetterVillages;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
                 DeferredRegister.create(ForgeRegistries.ITEMS, BetterVillages.MOD_ID);
 
+    public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
+
+
+    public static void  register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
