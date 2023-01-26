d11cceedd56748078672f5e7103d15b847d6147b
Prime-png <Primenotfound@gmail.com>
2023-01-25 19:47:41 -0600

    added a creative item tab for all blocks/items

---
+++ src/main/java/net/prime/bettervillages/item/ModCreativeModeTab.java
@@ -0,0 +1,13 @@
+package net.prime.bettervillages.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+
+public class ModCreativeModeTab {
+    public static final CreativeModeTab BETTER_VILLAGES = new CreativeModeTab("bettervillages") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(ModItems.ZIRCON.get());
+        }
+    };
+}
--- src/main/java/net/prime/bettervillages/item/ModItems.java
+++ src/main/java/net/prime/bettervillages/item/ModItems.java
@@ -1,6 +1,5 @@
 package net.prime.bettervillages.item;
 
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,7 +11,7 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
                 DeferredRegister.create(ForgeRegistries.ITEMS, BetterVillages.MOD_ID);
 
-    public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+    public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BETTER_VILLAGES)));
 
 
 
