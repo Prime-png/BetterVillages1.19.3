032f08aa4d58f17bffea92707e12880923e5a306
Prime-png <Primenotfound@gmail.com>
2023-02-11 12:59:09 -0600

    added a Helmet with a temporary texture that cant be worn

--- src/main/java/net/prime/bettervillages/item/ModItems.java
+++ src/main/java/net/prime/bettervillages/item/ModItems.java
@@ -12,7 +12,7 @@ public class ModItems {
                 DeferredRegister.create(ForgeRegistries.ITEMS, BetterVillages.MOD_ID);
 
     public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BETTER_VILLAGES)));
-
+    public static final RegistryObject<Item> ZIRCON_HELMET = ITEMS.register("zircon_helmet", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BETTER_VILLAGES).fireResistant().stacksTo(1)));
 
 
     public static void  register(IEventBus eventBus) {
