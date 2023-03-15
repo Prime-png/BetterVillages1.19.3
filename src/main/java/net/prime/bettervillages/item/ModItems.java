7a8bbf7587a5d30ca0375df51cb3c4772511771c
Prime-png <Primenotfound@gmail.com>
2023-03-15 15:38:44 -0500

    added a armour set with no texture

--- src/main/java/net/prime/bettervillages/item/ModItems.java
+++ src/main/java/net/prime/bettervillages/item/ModItems.java
@@ -1,5 +1,8 @@
 package net.prime.bettervillages.item;
 
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,7 +15,13 @@ public class ModItems {
                 DeferredRegister.create(ForgeRegistries.ITEMS, BetterVillages.MOD_ID);
 
     public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BETTER_VILLAGES)));
-    public static final RegistryObject<Item> ZIRCON_HELMET = ITEMS.register("zircon_helmet", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.BETTER_VILLAGES).fireResistant().stacksTo(1)));
+    private static net.minecraft.world.item.ArmorMaterial ArmorMaterial;
+    public static final RegistryObject<Item> ZIRCON_HELMET = ITEMS.register("zircon_helmet", () -> new ArmorItem(ModArmorMaterials.ZIRCON, EquipmentSlot.HEAD, new Item.Properties().tab(ModCreativeModeTab.BETTER_VILLAGES)));
+    public static final RegistryObject<Item> ZIRCON_CHESTPLATE = ITEMS.register("zircon_chestplate", () -> new ArmorItem(ModArmorMaterials.ZIRCON, EquipmentSlot.CHEST, new Item.Properties().tab(ModCreativeModeTab.BETTER_VILLAGES)));
+    public static final RegistryObject<Item> ZIRCON_LEGGINGS = ITEMS.register("zircon_leggings", () -> new ArmorItem(ModArmorMaterials.ZIRCON, EquipmentSlot.LEGS, new Item.Properties().tab(ModCreativeModeTab.BETTER_VILLAGES)));
+    public static final RegistryObject<Item> ZIRCON_BOOTS = ITEMS.register("zircon_boots", () -> new ArmorItem(ModArmorMaterials.ZIRCON, EquipmentSlot.FEET, new Item.Properties().tab(ModCreativeModeTab.BETTER_VILLAGES)));
+
+
 
 
     public static void  register(IEventBus eventBus) {
